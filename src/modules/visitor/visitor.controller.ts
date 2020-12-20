72ce4b86bbf7cb6e857b086a16d1cd24cbaa07bd
whitered932 <c0d3ar@ya.ru>
2020-12-20 17:27:55 +0500

    Created visitor API

--- src/modules/visitor/visitor.controller.ts
+++ src/modules/visitor/visitor.controller.ts
@@ -1,7 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { VisitorService } from './visitor.service';
+import { VisitorDto } from './visitor.dto';
 
 @Controller('visitor')
 export class VisitorController {
-  //  TODO: Relation with service
+  constructor(private visitorService: VisitorService) {}
+
+  @Get()
+  getAll() {
+    return this.visitorService.getAll();
+  }
+
+  @Get(':id')
+  getById(@Param('id') id: number) {
+    return this.visitorService.getById(id);
+  }
+
+  @Post('create')
+  create(@Body() visitor: VisitorDto) {
+    return this.visitorService.create(visitor);
+  }
+
+  @Put(':id/update')
+  update(@Param('id') id: number, @Body() visitor: VisitorDto) {
+    return this.visitorService.update(id, visitor);
+  }
+
+  @Delete(':id/delete')
+  delete(@Param('id') id: number) {
+    return this.visitorService.delete(id);
+  }
+
   //  TODO: API
 }
---
+++ src/modules/visitor/visitor.dto.ts
@@ -0,0 +1,4 @@
+export class VisitorDto {
+  name: string;
+  birthday: Date;
+}
